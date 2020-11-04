f01d0c29b4c9c80d5b45ea42f6235bc407dfa673
SaraGhonim <saratarek2020@gmail.com>
2020-11-05 00:05:31 +0200

    RNBottomSheet

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+import BottomSheet from 'reanimated-bottom-sheet';
+
+const App = () => {
+  const renderContent = () => (
+    <View
+      style={{
+        backgroundColor: 'white',
+        padding: 16,
+        height: 450,
+      }}>
+      <Text>Swipe down to close</Text>
+    </View>
+  );
+
+  const sheetRef = React.useRef(null);
+  return (
+    <>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'papayawhip',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Button
+          title="Open Bottom Sheet"
+          onPress={() => sheetRef.current.snapTo(0)}
+        />
+      </View>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[450, 300, 0]}
+        borderRadius={10}
+        renderContent={renderContent}
+      />
+    </>
+  );
+};
+
+export default App;
