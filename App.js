7e24759016ad04d05a4a8f716ed21a42c2a89078
SaraGhonim <saratarek2020@gmail.com>
2020-11-05 19:08:52 +0200

    RNTrackPlayer

--- App.js
+++ App.js
@@ -1,48 +1,63 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, Button, SafeAreaView} from 'react-native';
-import BottomSheet from 'reanimated-bottom-sheet';
+import TrackPlayer from 'react-native-track-player';
 
 const App = () => {
-  const renderContent = () => (
-    <View
-      style={{
-        backgroundColor: 'white',
-        padding: 16,
-        height: '100%',
-      }}>
-      <Text>Swipe down to close</Text>
-    </View>
-  );
+  const trackPlayerInit = async () => {
+    await TrackPlayer.setupPlayer();
+    await TrackPlayer.add({
+      id: '1',
+      url:
+        'http://docs.google.com/uc?export=open&id=18_kcR9izZi1ty-Cj5Yj-43h5GWnl627m',
+      type: 'default',
+      title: 'Surat Maryam',
+      album: 'My Album',
+      artist: 'a7med ell7dan',
+      artwork: 'https://picsum.photos/100',
+    });
+    return true;
+  };
+  const [isTrackPlayerInit, setIsTrackPlayerInit] = useState(false);
 
-  const sheetRef = React.useRef(null);
+  //initialize the TrackPlayer when the App component is mounted
+  useEffect(() => {
+    const startPlayer = async () => {
+      let isInit = await trackPlayerInit();
+      setIsTrackPlayerInit(isInit);
+    };
+    startPlayer();
+  }, []);
+
+  const play = () => {
+    TrackPlayer.play();
+  };
+  const pause = () => {
+    TrackPlayer.pause();
+  };
+
   return (
     <>
       <SafeAreaView
         style={{
           flex: 1,
-          backgroundColor: 'papayawhip',
+          backgroundColor: 'red',
         }}>
         <View
           style={{
             flex: 1,
             backgroundColor: 'papayawhip',
           }}>
+          <Text>Music Player</Text>
+          <Button title="Play" onPress={play} disabled={!isTrackPlayerInit} />
+          <Text>Music Player</Text>
+
           <Button
-            title="Open Bottom Sheet"
-            onPress={() => sheetRef.current.snapTo(0)}
-          />
-          <Button
-            title="close Bottom Sheet"
-            onPress={() => sheetRef.current.snapTo(1)}
+            style={{margin: 10, padding: 10}}
+            title="Pause"
+            onPress={pause}
           />
         </View>
       </SafeAreaView>
-      <BottomSheet
-        ref={sheetRef}
-        snapPoints={['90%', 100]}
-        borderRadius={10}
-        renderContent={renderContent}
-      />
     </>
   );
 };
